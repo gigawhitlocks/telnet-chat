48ea8d12ec53bf3b87eecfc8357198765b1b652c
Ian Whitlock <ian@theknown.net>
2013-08-06 17:23:20 -0400

    stopping point, ANSI and shit man

--- telnet-chat.go
+++ telnet-chat.go
@@ -6,7 +6,34 @@ import (
   "os"
   "bufio"
   "container/list"
+  "bytes"
+  //"unicode/utf8"
 )
+
+
+var bgcolors = map[string]string {
+  "red" : "\033[41m",
+  "yellow" : "\033[43m",
+  "green" : "\033[42m",
+  "blue" : "\033[46m",
+  "indigo" : "\033[44m",
+  "violet" : "\033[45m",
+  "black" : "\033[40m",
+  "default" : "\033[49m",
+}
+
+var fgcolors = map[string]string {
+  "black": "\033[30m",
+  "white": "\033[37m",
+  "red" : "\033[31m",
+  "yellow" : "\033[33m",
+  "green" : "\033[32m",
+  "blue" : "\033[36m",
+  "indigo" : "\033[34m",
+  "violet" : "\033[35m",
+  "default" : "\033[39m",
+}
+
 
 var clients = list.New()
 
@@ -24,18 +51,33 @@ func main() {
 
 func handleConnection ( conn net.Conn ) {
   connp := clients.PushFront(conn)
-  defer clients.Remove(connp)
+  defer func () {
+    clients.Remove(connp)
+  }()
 
   fmt.Println("connection handled")
   for {
-    status, _ := bufio.NewReader(conn).ReadString('\n')
-    fmt.Printf("%s",status)
+    input, _ := bufio.NewReader(conn).ReadString('\n')
+    fmt.Printf("%s",input)
     for c := clients.Front(); c != nil; c = c.Next() {
+        var b bytes.Buffer
+        b.Write([]byte("\033[42m"))
+        for i := 0; i < 80/*utf8.RuneCountInString(input)*/ ; i++ {
+          // do stuff
+          b.Write([]byte(" "))
+        }
+        b.Write([]byte("\033[49m"))
+        writer := bufio.NewWriter((c.Value).(net.Conn))
+        writer.WriteString(b.String())
+        writer.Flush()/*
       if c != connp {
         writer := bufio.NewWriter((c.Value).(net.Conn))
-        writer.WriteString(status)
+        writer.WriteString(input)
         writer.Flush()
-      }
+      }*/
     }
   }
 }
+
+
+
