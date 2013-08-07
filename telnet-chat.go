e36eab86b63d9a60ffa70b2d78790a05623c6dd4
Ian Whitlock <ian@theknown.net>
2013-08-07 12:40:15 -0400

    prints rainbows

--- telnet-chat.go
+++ telnet-chat.go
@@ -1,54 +1,86 @@
 package main
 
 import (
-  "net"
-  "fmt"
-  "os"
-  "bufio"
-  "container/list"
-  "bytes"
-  //"unicode/utf8"
+	"bufio"
+	"bytes"
+	"container/list"
+	"fmt"
+	"net"
+	"os"
+	//"unicode/utf8"
 )
 
-
-var bgcolors = map[string]string {
-  "red" : "\033[41m",
-  "yellow" : "\033[43m",
-  "green" : "\033[42m",
-  "blue" : "\033[46m",
-  "indigo" : "\033[44m",
-  "violet" : "\033[45m",
-  "black" : "\033[40m",
-  "default" : "\033[49m",
+var bgcolors = map[string]string{
+	"red":     "\033[41m",
+	"yellow":  "\033[43m",
+	"green":   "\033[42m",
+	"blue":    "\033[46m",
+	"indigo":  "\033[44m",
+	"violet":  "\033[45m",
+	"black":   "\033[40m",
+	"default": "\033[49m",
 }
 
-var fgcolors = map[string]string {
-  "black": "\033[30m",
-  "white": "\033[37m",
-  "red" : "\033[31m",
-  "yellow" : "\033[33m",
-  "green" : "\033[32m",
-  "blue" : "\033[36m",
-  "indigo" : "\033[34m",
-  "violet" : "\033[35m",
-  "default" : "\033[39m",
+var fgcolors = map[string]string{
+	"black":   "\033[30m",
+	"white":   "\033[37m",
+	"red":     "\033[31m",
+	"yellow":  "\033[33m",
+	"green":   "\033[32m",
+	"blue":    "\033[36m",
+	"indigo":  "\033[34m",
+	"violet":  "\033[35m",
+	"default": "\033[39m",
 }
-
 
 var clients = list.New()
 
 func main() {
-  ln, err := net.Listen("tcp", ":6667")
-  if err != nil {
-    fmt.Fprintf(os.Stderr, "Something went wrong before loop: %s", err)
-  }
-  for {
-    conn, _ := ln.Accept()
-    go handleConnection(conn)
+	ln, err := net.Listen("tcp", ":1337")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Something went wrong before loop: %s", err)
+	}
+	for {
+		conn, _ := ln.Accept()
+		go handleConnection(conn)
 
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	connp := clients.PushFront(conn)
+	defer func() {
+		clients.Remove(connp)
+	}()
+
+  for {
+    fmt.Println("connection handled")
+    for c := clients.Front(); c != nil; c = c.Next() {
+      if c != connp {
+        paintRainbow((c.Value).(net.Conn))
+
+      }
+    }
   }
 }
 
+func paintRainbow(client net.Conn) {
+	var buf bytes.Buffer
+	for _, color := range bgcolors {
+		buf.Write([]byte(color))
+		for i := 0; i < 80; i++ {
+			buf.Write([]byte(" "))
+		}
+		buf.Write([]byte(bgcolors["default"]))
+		buf.Write([]byte("\n"))
+	}
+	writer := bufio.NewWriter(client)
+	writer.WriteString(buf.String())
+	writer.Flush()
+
+}
+
+/*
 func handleConnection ( conn net.Conn ) {
   connp := clients.PushFront(conn)
   defer func () {
@@ -62,8 +94,9 @@ func handleConnection ( conn net.Conn ) {
     for c := clients.Front(); c != nil; c = c.Next() {
         var b bytes.Buffer
         b.Write([]byte("\033[42m"))
-        for i := 0; i < 80/*utf8.RuneCountInString(input)*/ ; i++ {
+        for i := 0; i < 80/*utf8.RuneCountInString(input) ; i++ {
           // do stuff
+          /*
           b.Write([]byte(" "))
         }
         b.Write([]byte("\033[49m"))
@@ -74,10 +107,10 @@ func handleConnection ( conn net.Conn ) {
         writer := bufio.NewWriter((c.Value).(net.Conn))
         writer.WriteString(input)
         writer.Flush()
-      }*/
+      }
     }
   }
 }
 
 
-
+*/
